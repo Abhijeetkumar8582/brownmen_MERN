8602cf7c0a29959c94b192c5fa4888df0fad7341
Abhijeet kumar <abhijeet122kumar@gmail.com>
2023-04-14 22:00:06 +0530

    production check in netlify2

--- Desktop/Brownmen/client/pages/articles/category.js
+++ Desktop/Brownmen/client/pages/articles/category.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import  { useEffect } from 'react'
+
 
 
 
